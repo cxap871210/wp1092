fba0854c3abe379c82ece5b2ee587840d614e8ae
Ray Chen <b07705052@ntu.edu.tw>
2021-05-08 18:00:22 +0800

    hw 5

---
+++ hw5/own/server/core/getNumber.js
@@ -0,0 +1,19 @@
+let number
+
+function getRandom(min,max){
+  return Math.floor(Math.random()*(max-min+1))+min;
+};
+
+
+const getNumber = (forceRestart = false) => {
+  // TODO:
+  // generate a random number if number is undefined or forceRestart is true
+  if (forceRestart === true | typeof(number) === "undefined")
+  {
+    number = getRandom(0,100)
+  }
+  // console.log('getNumber:', number)
+  return number
+}
+
+export default getNumber
