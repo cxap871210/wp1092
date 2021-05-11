3cd03cf32ced54c6a4195a47d4f377605648efec
Ray Chen <b07705052@ntu.edu.tw>
2021-05-11 22:17:46 +0800

    review

--- hw5/review/2/server/core/getNumber.js
+++
@@ -1,9 +0,0 @@
-let number
-
-const getNumber = (forceRestart = false) => {
-  // TODO:
-  // generate a random number if number is undefined or forceRestart is true
-  return number
-}
-
-export default getNumber
